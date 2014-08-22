2b7668b3c0f9deadd4c36f0e5bdd75675e07aaa1
Matthew Sell <matt.sell@fluke.com>
2014-08-22 14:31:16 -0700

    Ignoring AbstractTest class for unit testing - JUnit should only executes subclasses in this case

--- mass-core/src/test/java/edu/uw/bothell/css/dsl/MASS/AbstractTest.java
+++ mass-core/src/test/java/edu/uw/bothell/css/dsl/MASS/AbstractTest.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import org.easymock.EasyMockRunner;
 import org.easymock.EasyMockSupport;
 import org.junit.After;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 /**
@@ -13,6 +14,7 @@ import org.junit.runner.RunWith;
  * @author Matthew Sell
  *
  */
+@Ignore
 @RunWith(EasyMockRunner.class)
 public class AbstractTest extends EasyMockSupport {
 
