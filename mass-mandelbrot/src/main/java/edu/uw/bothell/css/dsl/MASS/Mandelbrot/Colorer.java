89b8f7c63fa7be179c2c2ca0a4cad49613054982
HungHo <hohung@microsoft.com>
2015-01-17 21:41:58 -0800

    mandelbrot synchronous mode sample

---
+++ mass-mandelbrot/src/main/java/edu/uw/bothell/css/dsl/MASS/Mandelbrot/Colorer.java
@@ -0,0 +1,91 @@
+package edu.uw.bothell.css.dsl.MASS.Mandelbrot;
+
+import java.net.InetAddress;
+
+import edu.uw.bothell.css.dsl.MASS.Agent;
+
+public class Colorer extends Agent {
+
+  private Object obj;
+
+  public static final int CALCULATE_COLOR = 0;
+  public static final int MIGRATE = 1;
+  public static final int INIT_MIGRATE = 2;
+
+  /**
+   * This constructor will be called upon instantiation by MASS The Object
+   * supplied MAY be the same object supplied when Places was created
+   * 
+   * @param obj
+   */
+  public Colorer(Object obj) {
+    this.obj = obj;
+  }
+
+  /**
+   * This method is called when "callAll" is invoked from the master node
+   */
+  public Object callMethod(int method, Object o) {
+
+    switch (method) {
+
+    case CALCULATE_COLOR:
+      return calculateColor(o);
+
+    case MIGRATE:
+      return move(o);
+    case INIT_MIGRATE:
+      return initMigrate(o);
+    default:
+      return new String("Unknown Method Number: " + method);
+
+    }
+
+  }
+
+  private Object initMigrate(Object o) {
+    int yModifier = (Integer)o;
+    migrate(0, yModifier);
+    return o;
+  }
+
+  /**
+   * Calculate the color of the current place in the Matrix
+   * http://en.wikipedia.org/wiki/Mandelbrot_set#Computer_drawings
+   * 
+   * @param o
+   * @return
+   */
+  public Object calculateColor(Object o) {
+    int xModifier = this.getPlace().getIndex()[0];
+    int yModifier = this.getPlace().getIndex()[1];
+    double x0 = -2.5 + (yModifier / Program.MATRIX_SIZE) * 1.5;
+    double y0 = -1.0 + (xModifier / Program.MATRIX_SIZE) * 2.0;
+    double x = 0.0, y = 0.0;
+    int iteration = 0;
+    while (x * x + y * y < 2 * 2 && iteration < Program.MAX_ITERATION) {
+      double xtemp = x * x - y * y + x0;
+      y = 2 * x * y + y0;
+      x = xtemp;
+      iteration++;
+    }
+    return new Integer(iteration);
+  }
+
+  /**
+   * Move this Agent to the next position in the X-coordinate
+   * 
+   * @param o
+   * @return
+   */
+  public Object move(Object o) {
+
+    int xModifier = this.getPlace().getIndex()[0];
+    int yModifier = this.getPlace().getIndex()[1];
+    xModifier++;
+
+    migrate(xModifier, yModifier);
+    return o;
+  }
+
+}
--- mass-mandelbrot/src/main/java/edu/uw/bothell/css/dsl/MASS/Mandelbrot/Program.java
+++ mass-mandelbrot/src/main/java/edu/uw/bothell/css/dsl/MASS/Mandelbrot/Program.java
@@ -8,6 +8,7 @@ public class Program {
 
 	private static final String NODE_FILE = "nodes.xml";
 	private static final String JAR_FILE_NAME = "mass-quickstart-0.8.2-SNAPSHOT.jar";
+	public static final int MATRIX_SIZE = 200, MAX_ITERATION = 200;
 	
 	public static void main(String[] args) {
 
@@ -15,45 +16,54 @@ public class Program {
 		MASS.addLibrary(JAR_FILE_NAME);
 		MASS.setNodeFilePath(NODE_FILE);
 		
+		double[][] colors = new double[MATRIX_SIZE][MATRIX_SIZE];
+		
 		// start MASS
 		MASS.init();
 		
-		// create all Places (having dimensions of x, y, and z)
-		// the total number of Place objects that will be created is x*y*z
-		int x = 5;
-		int y = 5;
-		int z = 5;
-		Places places = new Places(1, "edu.uw.bothell.css.dsl.MASS.QuickStart.Matrix", (Object) new Integer(0), x, y, z);
+		Places places = new Places(1, "edu.uw.bothell.css.dsl.MASS.Mandelbrot.Matrix", (Object) new Integer(0), MATRIX_SIZE, MATRIX_SIZE);
 		
-		// as a test, instruct all places to return the hostnames of the machines where they are located
-		Object[] placeCallAllObjs = new Object[x*y*z];
-		Object[] calledPlacesResults = (Object[]) places.callAll(Matrix.GET_HOSTNAME, placeCallAllObjs);
-		
-		// create Agents (number of Agents = x*y in this case), in Places
-		Agents agents = new Agents(1, "edu.uw.bothell.css.dsl.MASS.QuickStart.Nomad", null, places, x * y);
-		Object[] agentsCallAllObjs = new Object[x*y];
-		Object[] calledAgentsResults = (Object[]) agents.callAll(Nomad.GET_HOSTNAME, agentsCallAllObjs);
+		// create Agents (number of Agents = y in this case), in Places
+		Agents agents = new Agents(1, "edu.uw.bothell.css.dsl.MASS.Maldelbrot.Colorer", null, places, MATRIX_SIZE);
+		Object[] agentsCallAllObjs = new Object[MATRIX_SIZE];
+		for(int i = 0; i < MATRIX_SIZE; i++){
+		  agentsCallAllObjs[i] = i;
+		}
+		Object[] calledAgentsResults = (Object[]) agents.callAll(Colorer.INIT_MIGRATE, agentsCallAllObjs);
+		agents.manageAll();
+		calledAgentsResults = (Object[]) agents.callAll(Colorer.CALCULATE_COLOR, agentsCallAllObjs);
+		for(int i = 0; i < MATRIX_SIZE; i++)
+		{
+		  colors[0][i] = (double)calledAgentsResults[i];
+		}
 		
 		// move all Agents four times to cover all dimensions in Places
-		for (int i = 0; i < 4; i ++) {
+		for (int i = 1; i < MATRIX_SIZE; i ++) {
 			
 			// tell Agents to move
-			agents.callAll(Nomad.MIGRATE);
+			agents.callAll(Colorer.MIGRATE);
 			
 			// sync all Agent status
 			agents.manageAll();
 			
 			// find out where they live now
-			calledAgentsResults = (Object[]) agents.callAll(Nomad.GET_HOSTNAME, agentsCallAllObjs);
-			
+			calledAgentsResults = (Object[]) agents.callAll(Colorer.CALCULATE_COLOR, agentsCallAllObjs);
+			for(int j = 0; j < MATRIX_SIZE; j++)
+	    {
+	      colors[i][j] = (double)calledAgentsResults[j];
+	    }
 		}
-		
-		// find out where all of the Agents wound up when all movements complete
-		calledAgentsResults = (Object[]) agents.callAll(Nomad.GET_HOSTNAME, agentsCallAllObjs);
 		
 		// orderly shutdown
 		MASS.finish();
-		
+		System.out.println("Result is :");
+		for(int i = 0; i < MATRIX_SIZE; i++){
+		  for(int j = 0; j < MATRIX_SIZE; j++)
+		  {
+		    System.out.print(colors[i][j] + " ");
+		  }
+		  System.out.println();
+		}
 	 }
 	 
 }
