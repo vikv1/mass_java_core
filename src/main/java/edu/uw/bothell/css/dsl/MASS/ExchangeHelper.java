5adf2e53953e7f8372dd7ceece939dd7ab7f1be0
Matthew Sell <matt.sell@fluke.com>
2015-06-29 15:19:00 -0700

    Removed commented-out code from ExchangeHelper

--- src/main/java/edu/uw/bothell/css/dsl/MASS/ExchangeHelper.java
+++ src/main/java/edu/uw/bothell/css/dsl/MASS/ExchangeHelper.java
@@ -1,13 +1,18 @@
 package edu.uw.bothell.css.dsl.MASS;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Vector;
-import java.net.*;
-import java.io.*;
 
 public class ExchangeHelper {
 
-    //@SuppressWarnings("unused")
-	//private static Socket socket;
     private static Socket[] sockets;
     private static InputStream[] inputs;
     private static OutputStream[] outputs;
