6ba8b3e46a69b0fd619d8c1f6adf954beeff1d87
Matthew Sell <matthew.r.sell@gmail.com>
2016-01-21 17:29:12 -0800

    Added unit test for making sure a JSCH connection exception is properly handled

--- src/test/java/edu/uw/bothell/css/dsl/MASS/UtilitiesTest.java
+++ src/test/java/edu/uw/bothell/css/dsl/MASS/UtilitiesTest.java
@@ -35,14 +35,17 @@ import static org.easymock.EasyMock.expect;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.easymock.Capture;
 import org.easymock.Mock;
 import org.easymock.TestSubject;
 import org.junit.Test;
 
+import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import com.jcraft.jsch.UserInfo;
 
@@ -112,5 +115,33 @@ public class UtilitiesTest extends AbstractTest {
 		ui.showMessage( randomString( 32 ) );
 		
 	}
+	
+	@Test
+	public void handleConnectionException() throws Exception {
+
+		Channel returnChannel = null;
+
+		expect( mockJsch.getSession("username", "host", 22) ).andThrow(new JSchException());
+
+		// put mocks into replay mode
+		replayAll();
+
+		try {
+			
+			returnChannel = utilities.LaunchRemoteProcess("host", 22, null, "username", null);
+			
+		}
+		
+		catch(Exception e) {
+			
+			// we NOT expect an exception
+			fail("Should have swallowed a connection exception!");
+			
+		}
+		
+		assertNull(returnChannel);
+		
+	}
+	
 	
 }
