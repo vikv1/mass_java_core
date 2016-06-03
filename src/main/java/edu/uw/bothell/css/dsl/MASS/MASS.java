d32fd5695bdf2c511559c4ead6da0b4755c7c851
Matthew Sell <matthew.r.sell@gmail.com>
2016-06-03 13:59:23 -0700

    Messed up classpath building - double-entry of "*.jar"

--- src/main/java/edu/uw/bothell/css/dsl/MASS/MASS.java
+++ src/main/java/edu/uw/bothell/css/dsl/MASS/MASS.java
@@ -440,7 +440,6 @@ public class MASS extends MASSBase {
     		// add MASS home directory itself as part of the classpath
    			if (node.getMassHome() != null) {
 	    		commandBuilder.append("-cp " + node.getMassHome() + "/*.jar ");
-	    		commandBuilder.append("/*.jar ");
    			}
 
     		// MProcess and its arguments
