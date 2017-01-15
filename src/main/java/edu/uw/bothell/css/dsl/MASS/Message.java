12213f0d6477b3ab32cceaa4a3a4315286d1a5ba
Utku Mert <utkumert@gmail.com>
2017-01-15 12:40:03 -0800

    Message.java: unnecessary async var removed.

--- src/main/java/edu/uw/bothell/css/dsl/MASS/Message.java
+++ src/main/java/edu/uw/bothell/css/dsl/MASS/Message.java
@@ -60,11 +60,7 @@ public class Message implements Serializable {
 	    AGENTS_CALL_ALL_VOID_OBJECT,              // 13            
 	    AGENTS_CALL_ALL_RETURN_OBJECT,            // 14            
 	    AGENTS_MANAGE_ALL,                        // 15            
-	    AGENTS_MIGRATION_REMOTE_REQUEST,          // 16  
-	    
-	    /** Async section **/
-    	// 21 tell master that I'm done
-	    NODE_COMPLETE_NOTIFY_SOURCE("NODE_COMPLETE_NOTIFY_SOURCE");
+	    AGENTS_MIGRATION_REMOTE_REQUEST;          // 16
     	
     	private final String value;
     	
