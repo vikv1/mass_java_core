eaa201955333b96463a2ade957e9ec7953d9248f
Utku Mert <utkumert@gmail.com>
2017-01-24 17:36:42 -0800

    New classes, AgentSpawnRequest and AgentSpawnRequestManager are introduced.

---
+++ src/main/java/edu/uw/bothell/css/dsl/MASS/AgentSpawnRequest.java
@@ -0,0 +1,37 @@
+package edu.uw.bothell.css.dsl.MASS;
+
+/**
+ * Created by utku on 1/24/17.
+ *
+ * This class simply contains the serialized version of the new agent object
+ *  and the coordinates that new agent will become active.
+ */
+public class AgentSpawnRequest
+{
+
+    // Serialized agent object
+    // private KRYOOBJECT serializedAgent;
+
+    // Coordinates
+    private int x, y;
+
+    
+
+    // Getters
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    // Setters
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
---
+++ src/main/java/edu/uw/bothell/css/dsl/MASS/AgentSpawnRequestManager.java
@@ -0,0 +1,14 @@
+package edu.uw.bothell.css.dsl.MASS;
+
+/**
+ * Created by utku on 1/24/17.
+ *
+ * In each computing node, this class is responsible for evaluating agent spawn requests
+ *  and deciding that whether they should be actively running in the system or they
+ *  should be serialized and stored in the queue after allocation.
+ */
+public class AgentSpawnRequestManager
+{
+    // Queue for agent spawn requests
+    //Queue<Integer> myQ=new LinkedList<Integer>();
+}
