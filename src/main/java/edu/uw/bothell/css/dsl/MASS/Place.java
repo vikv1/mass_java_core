40feb6cceccd6dc6939ac98baca114badd8e8337
Michael O'Keefe <mgok@uw.edu>
2017-08-28 18:36:04 -0700

    Removed commented out code involving hadoop from Place.java

--- src/main/java/edu/uw/bothell/css/dsl/MASS/Place.java
+++ src/main/java/edu/uw/bothell/css/dsl/MASS/Place.java
@@ -209,39 +209,6 @@ public class Place {
 		return netcdfFile.read(variableToRead, getPlaceOrderPerNode());
 	}
 
-//	public void readFilefromHDFS(String file) throws IOException {
-//		Configuration conf = new Configuration();
-//		conf.addResource(new org.apache.hadoop.fs.Path("/home/hadoop/hadoop/conf/core-site.xml"));
-//		conf.addResource(new org.apache.hadoop.fs.Path("/home/hadoop/hadoop/conf/hdfs-site.xml"));
-//		conf.addResource(new org.apache.hadoop.fs.Path("/home/hadoop/hadoop/conf/mapred-site.xml"));
-//
-//		FileSystem fileSystem = FileSystem.get(conf);
-//
-//		org.apache.hadoop.fs.Path path = new org.apache.hadoop.fs.Path(file);
-//		if (!fileSystem.exists(path)) {
-//			System.out.println("File " + file + " does not exists");
-//			return;
-//		}
-//
-//		FSDataInputStream in = fileSystem.open(path);
-//
-//		String filename = file.substring(file.lastIndexOf('/') + 1,
-//				file.length());
-//
-//		OutputStream out = new BufferedOutputStream(new FileOutputStream(
-//				new File(filename)));
-//
-//		byte[] b = new byte[1024];
-//		int numBytes = 0;
-//		while ((numBytes = in.read(b)) > 0) {
-//			out.write(b, 0, numBytes);
-//		}
-//
-//		in.close();
-//		out.close();
-//		fileSystem.close();
-//	}
-
 	/**
 	 * Gets the file attribute from the file table
 	 * @param fileDescriptor unique identifier for the file attribute to return
