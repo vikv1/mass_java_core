ceb892f04ae4a4b217ff2bbee30315d5de7d3c8e
Matthew Sell <mrsell@uw.edu>
2017-10-23 11:08:06 -0700

    Added "OFF" as a valid logger level to disable all logging for performance [Issue #96]

--- src/main/java/edu/uw/bothell/css/dsl/MASS/logging/LogLevel.java
+++ src/main/java/edu/uw/bothell/css/dsl/MASS/logging/LogLevel.java
@@ -1,11 +1,28 @@
 package edu.uw.bothell.css.dsl.MASS.logging;
 
+/**
+ * Enumerations for MASS message logging
+ */
 public enum LogLevel {
 
+	/**
+	 * Record messages for events generating exceptions or result in system instability
+	 */
+	ERROR,
+
+	/**
+	 * Messages used for debugging and troubleshooting (lowest log level)
+	 */
 	DEBUG,
 	
-	ERROR,
+	/**
+	 * Disable all message logging (highest log level)
+	 */
+	OFF,
 	
+	/**
+	 * Messages for events that are not immediate errors but could eventually lead to system instability or exceptions
+	 */
 	WARN
 	
 }
