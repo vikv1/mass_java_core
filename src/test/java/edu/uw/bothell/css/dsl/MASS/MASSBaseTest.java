c62e60449cda4352a807bffccc2f2be24360fc25
Matthew Sell <mrsell@uw.edu>
2017-10-26 09:46:25 -0700

    Added some initial unit tests for MASSBase

--- src/test/java/edu/uw/bothell/css/dsl/MASS/MASSBaseTest.java
+++ src/test/java/edu/uw/bothell/css/dsl/MASS/MASSBaseTest.java
@@ -30,10 +30,13 @@
 
 package edu.uw.bothell.css.dsl.MASS;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import org.easymock.TestSubject;
+import org.junit.After;
 import org.junit.Test;
 
 public class MASSBaseTest extends AbstractTest {
@@ -41,17 +44,93 @@ public class MASSBaseTest extends AbstractTest {
 	@TestSubject
 	private MASSBase massBase = new MASSBase();
 	
+	@After
+	public void tearDown() {
+
+		// perform normal cleanup activities
+		super.tearDown();
+		
+		// reset test subject fields
+		
+	}
+
 	@Test
 	@SuppressWarnings("static-access")
 	public void hasValidLogFilenameAutoDetectHostname() throws Exception {
-		
+
+		// testing in isolation...
+		MASSBase mb = new MASSBase();
+
 		// init without specifying a hostname in node config
-		massBase.initMASSBase(new MNode());
+		mb.initMASSBase(new MNode());
 		
 		// should generate a valid logging filename, with a valid host
-		String loggingFilename = massBase.getLogFileName();
+		String loggingFilename = mb.getLogFileName();
 		assertNotNull(loggingFilename);
 		assertFalse(loggingFilename.toLowerCase().contains("null"));
+		
+	}
+	
+	@Test
+	@SuppressWarnings("static-access")
+	public void getAllNodesPreInit() throws Exception {
+		
+		// testing in isolation...
+		MASSBase mb = new MASSBase();
+		
+		// should not have any nodes when first instantiated
+		assertEquals( 0, mb.getAllNodes().size() );
+		assertEquals( 0, mb.getHosts().size() );
+		assertEquals( 0, mb.getRemoteNodes().size() );
+		assertEquals( 0, mb.getSystemSize() );
+		assertNull( mb.getMasterNode() );
+		
+	}
+
+	@Test
+	@SuppressWarnings("static-access")
+	public void addNode() throws Exception {
+		
+		// testing in isolation...
+		MASSBase mb = new MASSBase();
+		
+		// add a single master node
+		MNode masterNode = new MNode();
+		masterNode.setHostName( "master" );
+		masterNode.setMaster( true );
+		mb.addNode( masterNode );
+		
+		// verify node representations
+		assertEquals( 1, mb.getAllNodes().size() );
+		assertEquals( 1, mb.getHosts().size() );
+		assertEquals( 0, mb.getRemoteNodes().size() );
+		assertEquals( 1, mb.getSystemSize() );
+		assertEquals( masterNode, mb.getMasterNode() );
+		
+		// master node should have PID of zero
+		assertEquals( 0, masterNode.getPid() );
+		
+		// add a remote node
+		MNode remoteNode = new MNode();
+		remoteNode.setHostName( "remote" );
+		mb.addNode( remoteNode );
+		
+		// verify node representations
+		assertEquals( 2, mb.getAllNodes().size() );
+		assertEquals( 2, mb.getHosts().size() );
+		assertEquals( 1, mb.getRemoteNodes().size() );
+		assertEquals( 2, mb.getSystemSize() );
+		assertEquals( masterNode, mb.getMasterNode() );
+
+		// remote node should have PID of one (auto incremented)
+		assertEquals( 1, remoteNode.getPid() );
+
+	}
+	
+	@Test
+	@SuppressWarnings("static-access")
+	public void getLogger() throws Exception {
+		assertNotNull( massBase.getLogger() );
 		
 	}
 
