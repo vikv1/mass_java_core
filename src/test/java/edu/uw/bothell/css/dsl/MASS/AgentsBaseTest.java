55ce226a45e1dc58dcf22650fe945fb95f96d684
Matthew Sell <mrsell@uw.edu>
2017-11-14 15:52:33 -0800

    Added unit tests for AgentsBase for non-threaded functionality

---
+++ src/test/java/edu/uw/bothell/css/dsl/MASS/AgentsBaseTest.java
@@ -0,0 +1,149 @@
+/*
+
+ 	MASS Java Software License
+	© 2012-2017 University of Washington
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	The following acknowledgment shall be used where appropriate in publications, presentations, etc.:      
+
+	© 2012-2015 University of Washington. MASS was developed by Computing and Software Systems at University of 
+	Washington Bothell.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+
+*/
+
+package edu.uw.bothell.css.dsl.MASS;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import edu.uw.bothell.css.dsl.MASS.logging.LogLevel;
+
+/**
+ * Perform a series of unit tests against the AgentsBase class to verify proper
+ * and consistent behavior of the class / methods
+ */
+public class AgentsBaseTest extends AbstractTest {
+
+	// class under test
+	private AgentsBase agentsBase;
+
+	private static PlacesBase placesBase;
+	private static final int PLACES_HANDLE = randomInt();
+	private static final int AGENTS_HANDLE = randomInt();
+	
+	@BeforeClass
+	public static void beforeAll() {
+		
+		// MASSBase should be made ready for use before any test runs
+		MNode masterNode = new MNode();
+		masterNode.setHostName( randomString() );
+		masterNode.setMaster( true );
+		MASSBase.addNode( masterNode );
+		MASSBase.initMASSBase( masterNode );
+
+		int[] placesMatrix = new int[]{ 1, 1, 1 };
+		placesBase = new PlacesBase( PLACES_HANDLE, SimpleTestPlace.class.getName(), 1, null, placesMatrix );
+		MASSBase.getPlacesMap().put( PLACES_HANDLE, placesBase );
+
+	}
+
+	@AfterClass
+	public static void afterAll() {
+		
+		// clean up MASSBase
+		MASSBase.getAllNodes().clear();
+		MASSBase.setCurrentArgument( null );
+		MASSBase.setCurrentFunctionId( 0 );
+		MASSBase.setCurrentMsgType( null );
+		MASSBase.setCurrentAgentsBase( null );
+		MASSBase.setCurrentAgentsBase( null );
+		MASSBase.setCurrentReturns( null );
+		MASSBase.setDestinationPlaces( null );
+		MASSBase.getPlacesMap().clear();
+		MASSBase.getRemoteNodes().clear();
+		MASSBase.getHosts().clear();
+		MASSBase.getLogger().setLogLevel(LogLevel.OFF);
+		
+	}
+
+	@Before
+	public void onSetUp() {
+		
+		// start with new instances for each test
+		agentsBase = new AgentsBase( AGENTS_HANDLE, SimpleTestAgent.class.getName(), null, PLACES_HANDLE, 1 );
+		
+	}
+
+	@Test
+	public void getClassName() throws Exception {
+		
+		assertEquals( SimpleTestAgent.class.getName(), agentsBase.getClassName() );
+		
+	}
+
+	@Test
+	public void getLocalPopulation() throws Exception {
+		
+		// TODO - two methods to return the same thing?
+		
+		// Only one place, so should be only one Agent present on this node
+		assertEquals( 1, agentsBase.getLocalPopulation() );
+		assertEquals( 1, agentsBase.nLocalAgents() );
+		
+	}
+	
+	@Test
+	public void getPlacesHandle() throws Exception {
+		
+		assertEquals( PLACES_HANDLE, agentsBase.getPlacesHandle() );
+		
+	}
+	
+	@Test
+	public void getInitPopulation() throws Exception {
+		
+		// Only one place, so should have been only one Agent present on this node
+		assertEquals( 1, agentsBase.getInitPopulation() );
+		
+	}
+	
+	@Test
+	public void getHandle() throws Exception {
+		
+		assertEquals( AGENTS_HANDLE, agentsBase.getHandle() );
+		
+	}
+	
+	@Test
+	public void getAgents() throws Exception {
+		
+		// not testing AgentList here - only that it can be retrieved from AgentsBase
+		assertNotNull( agentsBase.getAgents() );
+		
+	}
+	
+	
+	
+}
