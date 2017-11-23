0781c96c294ff0445af7585d5f851c248e074231
Matthew Sell <mrsell@uw.edu>
2017-11-22 17:10:31 -0800

    Added unit tests for classes in MassData package

---
+++ src/test/java/edu/uw/bothell/css/dsl/MASS/MassData/PreferencesTest.java
@@ -0,0 +1,103 @@
+/*
+
+ 	MASS Java Software License
+	© 2012-2017 University of Washington
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	The following acknowledgment shall be used where appropriate in publications, presentations, etc.:      
+
+	© 2012-2016 University of Washington. MASS was developed by Computing and Software Systems at University of 
+	Washington Bothell.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+
+*/
+
+package edu.uw.bothell.css.dsl.MASS.MassData;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.URI;
+
+import org.junit.Test;
+
+import edu.uw.bothell.css.dsl.MASS.AbstractTest;
+
+/**
+ * Perform a series of unit tests against the Preferences class to verify proper
+ * and consistent behavior of the class / methods
+ */
+public class PreferencesTest extends AbstractTest {
+
+	// class under test
+	private Preferences prefs = new Preferences();
+
+	@Test
+	public void getSetURI() throws Exception {
+	
+		URI uri = URI.create("https://prrt1steamlocomotivetrust.org");
+		
+		prefs.setURI( uri );
+		
+		assertEquals( uri, prefs.getUri() );
+		assertEquals( uri, prefs.getURI() );
+
+		// TODO - why are there two different URI get/set methods?
+		prefs.setUri( uri );
+
+		assertEquals( uri, prefs.getUri() );
+		assertEquals( uri, prefs.getURI() );
+
+	}
+	
+	@Test
+	public void getSetAutoConnect() throws Exception {
+	
+		boolean autoConnect = prefs.autoConnect();
+		
+		// toggle value
+		prefs.setAutoConnect( !autoConnect );
+		
+		assertEquals( !autoConnect, prefs.autoConnect() );
+		
+	}
+
+	@Test
+	public void getSetIsJava() throws Exception {
+	
+		boolean isJava = prefs.isJava();
+		
+		// toggle value
+		prefs.setJava( !isJava );
+		
+		assertEquals( !isJava, prefs.isJava() );
+		
+	}
+
+	@Test
+	public void getSetProgramDirectory() throws Exception {
+	
+		String directory = randomString();
+		
+		prefs.setProgramDirectory( directory );
+		
+		assertEquals( directory, prefs.getProgramDirectory() );
+		
+	}
+
+}
