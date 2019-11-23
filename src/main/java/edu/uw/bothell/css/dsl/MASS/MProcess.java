b08da01bf954e6d89e50101d11706e3682b9b575
Justin Gilroy <jegilroy@uw.edu>
2019-11-23 10:29:54 -0800

    Pass max agents to remote node

--- src/main/java/edu/uw/bothell/css/dsl/MASS/MProcess.java
+++ src/main/java/edu/uw/bothell/css/dsl/MASS/MProcess.java
@@ -118,10 +118,15 @@ public class MProcess {
 		int nThreads = Integer.parseInt(args[3]);
 		int serverPort = Integer.parseInt(args[4]);
 		String curDir = args[5];
+		int maxNumberOfAgents = Integer.parseInt(args[6]);
 
 		MASSBase.getLogger().setLogLevel(LogLevel.DEBUG);
 
 		MASSBase.getLogger().debug("MProcess - main");
+
+		AgentSerializer agentSerializer = AgentSerializer.getInstance();
+
+		agentSerializer.setMaxNumberOfAgents(maxNumberOfAgents);
 
 		MProcess mprocess = new MProcess(hostName, myPid, nProc, nThreads, serverPort, curDir);
 
