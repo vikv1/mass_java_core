e7ea83c0c3bbc321d3664fc80295c733cc1ad223
Justin Gilroy <jegilroy@uw.edu>
2020-01-16 17:23:05 -0800

    Add a few tests

--- src/test/java/edu/uw/bothell/css/dsl/MASS/graph/MATSimNetworkModelTest.java
+++ src/test/java/edu/uw/bothell/css/dsl/MASS/graph/MATSimNetworkModelTest.java
@@ -3,6 +3,8 @@ package edu.uw.bothell.css.dsl.MASS.graph;
 import edu.uw.bothell.css.dsl.MASS.MASS;
 import edu.uw.bothell.css.dsl.MASS.MNode;
 import edu.uw.bothell.css.dsl.MASS.Nodelist;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import javax.xml.bind.JAXBContext;
@@ -10,10 +12,17 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
 import java.io.File;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
 
 public class MATSimNetworkModelTest {
-    @Test
-    public void theNetworkIsImported() {
+    private MATSimNetworkModel network = null;
+
+    @Before
+    public void importNetwork() {
         String networkFilename = "../matsim/network-pt-simple.xml";
 
         File networkFile = new File(networkFilename);
@@ -23,19 +32,32 @@ public class MATSimNetworkModelTest {
 
             JAXBContext jaxbContext = JAXBContext.newInstance(MATSimNetworkModel.class);
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-            MATSimNetworkModel network = (MATSimNetworkModel) jaxbUnmarshaller.unmarshal(networkFile);
-
-            for (MATSimNetworkNode node : network.getNodes()) {
-                System.out.println(node);
-            }
+            network = (MATSimNetworkModel) jaxbUnmarshaller.unmarshal(networkFile);
         } catch (JAXBException e) {
-
             System.err.println( "Error initializing JAXB parser..." );
             e.printStackTrace();
 
             MASS.getLogger().error( "Error initializing JAXB parser...", e );
-            System.exit( -1 );
-
         }
+    }
+
+    @Test
+    public void theNetworkIsImported() {
+        assertNotNull(network);
+    }
+
+    @Test
+    public void theNetworkIsTheCorrectShape() {
+        assertEquals(6, network.getNodes().size());
+        assertEquals(5, network.getLinks().size());
+    }
+
+    @Test
+    public void theNetworkNodesAreIdentifiedCorrectly() {
+        List<Long> nodeIds = network.getNodes().stream().map(n -> n.id).collect(Collectors.toList());
+
+        Set<Long> nodesIdsSet = network.getNodes().stream().map(n -> n.id).collect(Collectors.toSet());
+
+        assertTrue(nodesIdsSet.containsAll(nodeIds));
     }
 }
