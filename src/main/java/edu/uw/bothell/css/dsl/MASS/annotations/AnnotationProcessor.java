a969d17dd749e4cafcabb3d6d256368b71491a72
ihfan <mrsell@uw.edu>
2020-11-10 11:33:54 -0800

    Updated copyright dates and added missing copyright headers in preparation for release

--- src/main/java/edu/uw/bothell/css/dsl/MASS/annotations/AnnotationProcessor.java
+++ src/main/java/edu/uw/bothell/css/dsl/MASS/annotations/AnnotationProcessor.java
@@ -1,7 +1,7 @@
 /*
 
  	MASS Java Software License
-	© 2012-2019 University of Washington
+	© 2012-2020 University of Washington
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
@@ -15,7 +15,7 @@
 
 	The following acknowledgment shall be used where appropriate in publications, presentations, etc.:      
 
-	© 2012-2019 University of Washington. MASS was developed by Computing and Software Systems at University of 
+	© 2012-2020 University of Washington. MASS was developed by Computing and Software Systems at University of 
 	Washington Bothell.
 
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -27,6 +27,7 @@
 	THE SOFTWARE.
 
 */
+
 package edu.uw.bothell.css.dsl.MASS.annotations;
 
 import java.lang.annotation.Annotation;
